51e02189fc7f953ce4277ccc9113eaee7b875fe2
Sanganu <kppkmsanu@gmail.com>
2019-11-10 23:09:55 -0700

    Local Storage

---
+++ README.md
@@ -0,0 +1,7 @@
+Weather API
+ -- Endpoints
+    1. Current Weather Forecast
+    2. 5 Day Forecast
+    3. UV Index
+
+-- Local Storage
--- sources/script.js
+++ sources/script.js
@@ -1,12 +1,21 @@
 //Local Storage
-var history = []
-history = JSON.parse(window.localStorage.getItem("history")) || [];
-for(let i=0;i<history.length;i++){
-    $("#list").append("<li>"+history[i]+"</li>")
-}
-$("#list").append(localStorage.getItem("recentweathersearch"))
 
-//SEarch
+
+// var history = JSON.parse(window.localStorage.getItem("history")) || [];
+var len = []
+var previouscities = JSON.parse(window.localStorage.getItem("weathersearchAPI"))|| [];
+
+console.log("SearchList",localStorage.weathersearchAPI);
+
+$("#list").empty();
+for(let i=0;i<previouscities.length;i++){
+    $("#list").append("<li>"+previouscities[i].city+"</li>")
+}
+
+
+// $("#list").append(localStorage.weathersearchAPI);
+
+//SEa
 $("#searchweather").on("click",function(event){
     event.preventDefault();
     var city = $("#entercity").val().trim() || "Denver"
@@ -17,24 +26,34 @@ $("#searchweather").on("click",function(event){
         datatype:"json",
         success: function(data){
             console.log(data);
-            console.log(history)
-            localStorage.setItem("recentweathersearch",city);
-            // if(history.length > 1){
-            //     if(history.indexOf(city) === -1){
-            //         history.push(city);
-            //         window.localStorage.setItem("history",JSON.stringify(history))
-            //     }
-            // }
-            // else{
-            //     history.push(city);
-            //     window.localStorage.setItem("history",JSON.stringify(history))
-            // }
+          if(localStorage.weathersearchAPI){
+            previouscities = JSON.parse(window.localStorage.getItem("weathersearchAPI"));
+            len = previouscities.length;
+          }
+          else{
+              len = 0;
+          }
+            console.log("The Previous cities",previouscities);
+            var found = false;
+            let i =0;
+           while(i<len && found === false){
+               if (previouscities[i].city === city){
+                   found = true;
+               }
+               i++;
+           }
+           console.log(found)
+           if(!found || len ===0){
+               previouscities.push({city});
+           }
+            window.localStorage.setItem("weathersearchAPI",JSON.stringify(previouscities));
+            $("#weathercontainer").empty();
+           
             var todate = new Date().toLocaleDateString()
-            console.log(data.weather);
-            console.log(data.main)
+        
             $("#weathercontainer").append(`
-            <div class=""><h3>${data.name}</h3><p>${todate}</p>
-            <h4>${data.wind.speed}MPH</h4><h5>${data.main.humidity}%</h5>
+            <div class=""><h5>${data.name}</h5><p>${todate}</p>
+            <h6>${data.wind.speed}MPH</h6><h6>${data.main.humidity}%</h6>
             <h6>${data.main.temp}°F</h6><img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/></div>`)
         }});
         getforecast(city);
@@ -52,7 +71,7 @@ function getforecast(cityname){
               $("#forecast").html("<div class='row'>");
               for(let i=0;i< data.list.length;i++){
                   if(data.list[i].dt_txt.endsWith("06:00:00")){
-                      $("#forecast").append(`<div class="col s6 m3"><div class="card cyan accent-1 darken-1"><h5>${data.list[i].dt_txt}</h5>
+                      $("#forecast").append(`<div class="col s6 m3"><div class="card cyan darken-1"><h5>${data.list[i].dt_txt}</h5>
                       <img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"/>
                       <p>${data.list[i].wind.speed}mph</p>
                       <p>${data.list[i].main.humidity}%</p><p>${data.list[i].main.temp}°F</p></div></div>`)
